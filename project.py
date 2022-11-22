cdeaa03d319ba09ef2e4365f01ebc4612b11edf7
Samantha <117988373+brisamaa@users.noreply.github.com>
2022-11-22 15:09:49 -0500

    Update project.py

--- project.py
+++ project.py
@@ -11,7 +11,7 @@ def download_data():
   gdown.download(url,output,quiet = False)
 
 download_data()
-data = pd.read_csv('data.csv', sep = ';', nrows = 1000000, parse_dates = ['ID','FECHA_CORTE'])
+data = pd.read_csv('data.csv', sep = ';', nrows = 1000000, parse_dates = ['FECHA_UTC','FECHA_CORTE'])
 st.dataframe(data.head(50))
 magnitud = data['MAGNITUD']
 st.line_chart(magnitud)
